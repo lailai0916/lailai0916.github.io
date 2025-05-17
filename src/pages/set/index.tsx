1193c36704b5d6be46cbf0a5ce579a3b0b321e28
lailai <lailai0x394@gmail.com>
2025-05-17 18:24:14 +0800

    upd

--- src/pages/set/index.tsx
+++ src/pages/set/index.tsx
@@ -1,6 +1,8 @@
+import React, { useEffect } from 'react';
 import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
+import confetti from 'canvas-confetti';
 
 import ColorGenerator from './_components/ColorGenerator';
 
@@ -13,10 +15,52 @@ function PageHeader() {
       <Heading as="h1">{TITLE}</Heading>
       <p>{DESCRIPTION}</p>
     </section>
-  )
+  );
+}
+
+function Confetti() {
+  const count = 200;
+  const defaults = {
+    origin: { y: 0.7 }
+  };
+
+  function fire(particleRatio: number, opts: confetti.Options) {
+    confetti({
+      ...defaults,
+      ...opts,
+      particleCount: Math.floor(count * particleRatio)
+    });
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 60,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
 }
 
 export default function setPage(): JSX.Element {
+  useEffect(() => {
+    Confetti();
+  }, []);
+
   return (
     <Layout title={TITLE} description={DESCRIPTION}>
       <main className="margin-vert--lg">
