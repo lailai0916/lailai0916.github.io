3b4abab9ddd13ece862669957a1c024d0b057d2a
lailai <lailai0x394@gmail.com>
2025-05-18 22:24:13 +0800

    upd

--- src/pages/set/index.tsx
+++ src/pages/set/index.tsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
-import confetti from 'canvas-confetti';
 
 import ColorGenerator from './_components/ColorGenerator';
+import Confetti from './_components/Confetti';
 
 const TITLE = '设置';
 const DESCRIPTION = '🎉 恭喜你发现了一个彩蛋页面 🥳';
@@ -16,44 +16,6 @@ function PageHeader() {
       <p>{DESCRIPTION}</p>
     </section>
   );
-}
-
-function Confetti() {
-  const count = 200;
-  const defaults = {
-    origin: { y: 0.7 }
-  };
-
-  function fire(particleRatio: number, opts: confetti.Options) {
-    confetti({
-      ...defaults,
-      ...opts,
-      particleCount: Math.floor(count * particleRatio)
-    });
-  }
-
-  fire(0.25, {
-    spread: 26,
-    startVelocity: 55,
-  });
-  fire(0.2, {
-    spread: 60,
-  });
-  fire(0.35, {
-    spread: 100,
-    decay: 0.91,
-    scalar: 0.8
-  });
-  fire(0.1, {
-    spread: 120,
-    startVelocity: 25,
-    decay: 0.92,
-    scalar: 1.2
-  });
-  fire(0.1, {
-    spread: 120,
-    startVelocity: 45,
-  });
 }
 
 export default function setPage(): JSX.Element {
