0d1f31c007612d60e630b3c6f96ce8d289c3b908
lailai <lailai0x394@gmail.com>
2025-06-25 16:49:49 +0800

    upd

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -5,11 +5,12 @@ import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 
 import BrowserWindow from '@site/src/components/BrowserWindow';
-import Notation from '@site/src/components/Notation';
-import Problem from '@site/src/components/Problem';
-import Desmos from '@site/src/components/BrowserWindow/Desmos';
 import IframeWindow from '@site/src/components/BrowserWindow/IframeWindow';
 import ImageWindow from '@site/src/components/BrowserWindow/ImageWindow';
+
+import Desmos from '@site/src/components/Desmos';
+import Notation from '@site/src/components/Notation';
+import Problem from '@site/src/components/Problem';
 
 export default {
   // Re-use the default mapping
