35fb3e63d28e99bd33e5be0ac4b2fda60d446e7b
lailai <lailai0x394@gmail.com>
2025-06-28 18:06:50 +0800

    Update index.tsx

--- src/pages/about/_components/Comment/index.tsx
+++ src/pages/about/_components/Comment/index.tsx
@@ -2,26 +2,25 @@ import React from "react";
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import Giscus from "@giscus/react";
 import { useColorMode } from "@docusaurus/theme-common";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 // https://zhuanlan.zhihu.com/p/687041162
 
 export default function Comment() {
   const { colorMode } = useColorMode();
+  const { i18n } = useDocusaurusContext();
 
   return (
-    <BrowserOnly fallback={<div>Loading Comment...</div>}>
+    <BrowserOnly fallback={<div>Loading comments...</div>}>
       {() => (
         <Giscus
           repo="lailai0916/giscus"
           repoId="R_kgDONHUoXA"
           category="Announcements"
           categoryId="DIC_kwDONHUoXM4Cjx_9"
-          mapping="pathname"
-          reactionsEnabled="1"
-          emitMetadata="0"
           inputPosition="top"
           theme={colorMode}
-          lang="zh-CN"
+          lang={i18n.currentLocale}
         />
       )}
     </BrowserOnly>
