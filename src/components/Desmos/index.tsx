c54d53118af39cf49aa703d0941a587281a673e0
lailai <lailai0x394@gmail.com>
2025-06-30 01:30:49 +0800

    Update index.tsx

--- src/components/Desmos/index.tsx
+++ src/components/Desmos/index.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 const Desmos = ({ url }: { url: string }) => {
   return (
-    <div>
-      <iframe
-        src={`https://www.desmos.com/calculator/${url}?embed`}
-        title="Desmos"
-        style={{
-          border: '1px solid #ccc',
-          width: 'min(100%, 400px)',
-          aspectRatio: '1 / 1',
-        }}
-      />
-    </div>
+    <iframe
+      src={`https://www.desmos.com/calculator/${url}?embed`}
+      title="Desmos"
+      style={{
+        border: '1px solid #ccc',
+        width: 'min(100%, 400px)',
+        aspectRatio: '1 / 1',
+      }}
+    />
   );
 };
 
