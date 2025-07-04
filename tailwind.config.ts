5be969b5cf04ed4c20322b9d4bec7bceb45afa4c
lailai <lailai0x394@gmail.com>
2025-07-04 19:09:10 +0800

    upd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
-import svgToDataUri from 'mini-svg-data-uri'
-import type { Config } from 'tailwindcss'
-import plugin from 'tailwindcss/plugin'
+import svgToDataUri from 'mini-svg-data-uri';
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
-const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette')
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette');
 
 const twConfig: Config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -51,29 +51,27 @@ const twConfig: Config = {
     plugin(({ matchUtilities, theme }) => {
       matchUtilities(
         {
-          'bg-grid': value => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
-            )}")`,
+          'bg-grid': (value) => ({
+            backgroundImage: `url("${svgToDataUri(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`)}")`,
           }),
         },
         {
           values: flattenColorPalette(theme('backgroundColor')),
           type: 'color',
-        },
-      )
+        }
+      );
     }),
     addVariablesForColors,
   ],
-}
+};
 
-export default twConfig
+export default twConfig;
 
 function addVariablesForColors({ addBase, theme }: any) {
-  const allColors = flattenColorPalette(theme('colors'))
-  const newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]))
+  const allColors = flattenColorPalette(theme('colors'));
+  const newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
 
   addBase({
     ':root': newVars,
-  })
+  });
 }
