b8fde86bf6056088175b8e03bdc7b0f3b25b2e31
lailai0916 <lailai0x394@gmail.com>
2025-07-30 22:57:11 +0800

    Update MDXComponents.tsx

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -1,29 +1,25 @@
-import React from 'react';
-// Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
+import DocCardList from '@theme/DocCardList';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
-import DocCardList from '@theme/DocCardList';
 
 import BrowserWindow from '@site/src/components/BrowserWindow';
 import IframeWindow from '@site/src/components/BrowserWindow/IframeWindow';
 import ImageWindow from '@site/src/components/BrowserWindow/ImageWindow';
 
-import Desmos from '@site/src/components/Desmos';
 import Notation from '@site/src/components/Notation';
+import Desmos from '@site/src/components/Desmos';
 import Problem from '@site/src/components/Problem';
 
 export default {
-  // Re-use the default mapping
   ...MDXComponents,
-  // Map tags to our components
+  DocCardList,
   Tabs,
   TabItem,
-  DocCardList,
   BrowserWindow,
   IframeWindow,
   ImageWindow,
-  Desmos,
   Notation,
+  Desmos,
   Problem,
 };
