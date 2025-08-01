ed0381c98d32709fa90c8ba3643122883b8e46c5
lailai0916 <lailai0x394@gmail.com>
2025-08-01 23:08:17 +0800

    Update QuickActions.tsx

--- src/pages/settings/_components/QuickActions.tsx
+++ src/pages/settings/_components/QuickActions.tsx
@@ -7,9 +7,7 @@ import styles from '../styles.module.css';
 
 function Confetti() {
   const count = 200;
-  const defaults = {
-    origin: { y: 0.7 },
-  };
+  const defaults = { origin: { y: 0.7 } };
 
   function fire(particleRatio: number, opts: confetti.Options) {
     confetti({
@@ -19,28 +17,11 @@ function Confetti() {
     });
   }
 
-  fire(0.25, {
-    spread: 26,
-    startVelocity: 55,
-  });
-  fire(0.2, {
-    spread: 60,
-  });
-  fire(0.35, {
-    spread: 100,
-    decay: 0.91,
-    scalar: 0.8,
-  });
-  fire(0.1, {
-    spread: 120,
-    startVelocity: 25,
-    decay: 0.92,
-    scalar: 1.2,
-  });
-  fire(0.1, {
-    spread: 120,
-    startVelocity: 45,
-  });
+  fire(0.25, { spread: 26, startVelocity: 55 });
+  fire(0.2, { spread: 60 });
+  fire(0.35, { spread: 100, decay: 0.91, scalar: 0.8 });
+  fire(0.1, { spread: 120, startVelocity: 25, decay: 0.92, scalar: 1.2 });
+  fire(0.1, { spread: 120, startVelocity: 45 });
 }
 
 function handleReset() {
