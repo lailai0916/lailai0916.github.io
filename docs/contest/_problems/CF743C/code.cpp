f5538cb3441420be75ca0e745418dc62d3fbe1b7
lailai0916 <lailai0x394@gmail.com>
2025-08-06 13:04:10 +0800

    upd

---
+++ docs/contest/_problems/CF743C/code.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll=long long;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n;
+	cin>>n;
+	if(n==1)
+	{
+		cout<<"-1"<<'\n';
+		return 0;
+	}
+	cout<<n<<' '<<n+1<<' '<<n*(n+1)<<'\n';
+	return 0;
+}
