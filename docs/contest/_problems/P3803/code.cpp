6a1e008657c83058de8a37495ea1c20d6dcffee9
lailai0916 <lailai0x394@gmail.com>
2025-08-07 10:48:50 +0800

    upd

--- docs/contest/_problems/P3803/code.cpp
+++ docs/contest/_problems/P3803/code.cpp
@@ -4,28 +4,28 @@ using namespace std;
 using Comp=complex<double>;
 const double pi=acos(-1.0);
 const int N=1<<20;
-Comp a[N<<1],b[N<<1],tmp[N<<1];
-int c[N<<1];
+Comp a[N<<1],b[N<<1],t[N<<1];
 void fft(Comp *f,int lim,int type)
 {
 	if(lim==1)return;
-	for(int i=0;i<lim;i++)tmp[i]=f[i];
-	for(int i=0;i<(lim>>1);i++)
+	int mid=lim>>1;
+	for(int i=0;i<lim;i++)t[i]=f[i];
+	for(int i=0;i<mid;i++)
 	{
-		f[i]=tmp[i<<1];
-		f[i+(lim>>1)]=tmp[(i<<1)+1];
+		f[i]=t[i<<1];
+		f[i+mid]=t[i<<1|1];
 	}
-	Comp *g=f,*h=f+(lim>>1);
-	fft(g,lim>>1,type);
-	fft(h,lim>>1,type);
-	Comp cur(1,0),step(cos(2*pi/lim),sin(2*pi/lim)*type);
-	for(int i=0;i<(lim>>1);i++)
+	Comp *g=f,*h=f+mid;
+	fft(g,mid,type);
+	fft(h,mid,type);
+	Comp cur(1,0),step(cos(pi*2/lim),sin(pi*2/lim)*type);
+	for(int i=0;i<mid;i++)
 	{
-		tmp[i]=g[i]+cur*h[i];
-		tmp[i+(lim>>1)]=g[i]-cur*h[i];
+		t[i]=g[i]+cur*h[i];
+		t[i+mid]=g[i]-cur*h[i];
 		cur*=step;
 	}
-	for(int i=0;i<lim;i++)f[i]=tmp[i];
+	for(int i=0;i<lim;i++)f[i]=t[i];
 }
 int main()
 {
@@ -51,7 +51,6 @@ int main()
 	fft(b,lim,1);
 	for(int i=0;i<lim;i++)a[i]*=b[i];
 	fft(a,lim,-1);
-	for(int i=0;i<=n+m;i++)c[i]=a[i].real()/lim+0.5;
-	for(int i=0;i<=n+m;i++)cout<<c[i]<<' ';
+	for(int i=0;i<=n+m;i++)cout<<int(a[i].real()/lim+0.5)<<' ';
 	return 0;
 }
