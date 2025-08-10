261ed410703e643a9ba444e265cbb9c736eafa24
lailai0916 <lailai0x394@gmail.com>
2025-08-10 23:47:45 +0800

    upd

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,15 +1,17 @@
 import React, { type ReactNode } from 'react';
 import Layout from '@theme/Layout';
 
+import Link from '@docusaurus/Link';
 import {
   PageTitle,
   PageMain,
   PageHeader,
 } from '@site/src/components/laikit/page';
 import DataCard from '@site/src/components/laikit/widget/DataCard';
+import styles from './styles.module.css';
 
 const TITLE = '地图';
-const DESCRIPTION = 'lailai\'s Home 网站地图';
+const DESCRIPTION = "lailai's Home 网站地图";
 
 function MapHeader() {
   return (
@@ -22,10 +24,90 @@ function MapHeader() {
   );
 }
 
-function MapContent() {
+function MapContainer() {
   return (
-    <div>
-      <h3>Map</h3>
+    <div className={styles.container}>
+      <div>
+        <h3>页面</h3>
+        <ul>
+          <li>
+            <Link to="/">首页</Link>
+          </li>
+          <li>
+            <Link to="/about">关于</Link>
+          </li>
+          <li>
+            <Link to="/travel">旅行</Link>
+          </li>
+          <li>
+            <Link to="/friends">友链</Link>
+          </li>
+          <li>
+            <Link to="/resources">资源</Link>
+          </li>
+          <li>
+            <Link to="/games">游戏</Link>
+          </li>
+          <li>
+            <Link to="/settings">设置</Link>
+          </li>
+          <li>
+            <Link to="/map">地图</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>文档</h3>
+        <ul>
+          <li>
+            <Link to="/docs/contest">竞赛</Link>
+          </li>
+          <li>
+            <Link to="/docs/note">笔记</Link>
+          </li>
+          <li>
+            <Link to="/docs/project">项目</Link>
+          </li>
+        </ul>
+        <h3>博客</h3>
+        <ul>
+          <li>
+            <Link to="/blog">博客</Link>
+          </li>
+          <li>
+            <Link to="/blog/archive">归档</Link>
+          </li>
+          <li>
+            <Link to="/blog/authors">作者</Link>
+          </li>
+          <li>
+            <Link to="/blog/tags">标签</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>语言</h3>
+        <ul>
+          <li>
+            <Link to="/map">English</Link>
+          </li>
+          <li>
+            <Link to="/zh-Hans/map">中文</Link>
+          </li>
+        </ul>
+        <h3>搜索</h3>
+        <ul>
+          <li>
+            <Link to="/search">搜索</Link>
+          </li>
+        </ul>
+        <h3>测试</h3>
+        <ul>
+          <li>
+            <Link to="/test">测试</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
@@ -35,7 +117,7 @@ export default function Map(): ReactNode {
     <Layout title={TITLE} description={DESCRIPTION}>
       <PageMain>
         <MapHeader />
-        <MapContent />
+        <MapContainer />
       </PageMain>
     </Layout>
   );
