7fd5f06c243eafe8b5d93cd85281c3e913f6b4f6
lailai0916 <lailai0x394@gmail.com>
2025-08-15 01:00:58 +0800

    upd

--- docs/contest/_problems/P2495/单调栈.cpp
+++ docs/contest/_problems/P2495/单调栈.cpp
@@ -47,7 +47,7 @@ bool cmp(int u,int v)
 	return dfn[u]<dfn[v];
 }
 vector<pair<int,int>> H[N];
-int h[N<<1];
+int h[N],s[N];
 bool tag[N];
 ll dp(int u)
 {
@@ -88,13 +88,27 @@ int main()
 			tag[h[i]]=1;
 		}
 		sort(h+1,h+k+1,cmp);
-		for(int i=1;i<k;i++)h[k+i]=lca(h[i],h[i+1]);
-		h[k<<=1]=1;
-		sort(h+1,h+k+1,cmp);
-		k=unique(h+1,h+k+1)-h-1;
-		for(int i=1;i<k;i++)
+		s[1]=1;
+		int t=1;
+		for(int i=1;i<=k;i++)
 		{
-			int u=lca(h[i],h[i+1]),v=h[i+1];
+			if(h[i]==1)continue;
+			int l=lca(h[i],s[t]);
+			while(t>1&&dep[s[t-1]]>=dep[l])
+			{
+				int u=s[t-1],v=s[t--];
+				H[u].push_back({v,dis[v]});
+			}
+			if(s[t]!=l)
+			{
+				H[l].push_back({s[t],dis[s[t]]});
+				s[t]=l;
+			}
+			s[++t]=h[i];
+		}
+		while(t>1)
+		{
+			int u=s[t-1],v=s[t--];
 			H[u].push_back({v,dis[v]});
 		}
 		cout<<dp(1)<<'\n';
