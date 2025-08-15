3b8d374be3f40e430676cb3073f3be5482c69712
lailai0916 <lailai0x394@gmail.com>
2025-08-15 20:27:44 +0800

    upd

--- src/pages/_components/NeuralNetwork/index.tsx
+++ src/pages/_components/NeuralNetwork/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NeuralNetworkInteractive from '@site/src/components/NeuralNetwork';
+import NeuralNetworkInteractive from '@site/src/pages/_components/NeuralNetwork/NeuralNetwork';
 import {
   SectionMain,
   SectionHeader,
@@ -9,23 +9,18 @@ import { translate } from '@docusaurus/Translate';
 export default function NeuralNetwork() {
   return (
     <SectionMain>
-      <div className="max-w-7xl mx-auto flex flex-col px-5">
-        <SectionHeader
-          title={translate({
-            id: 'home.neuralnetwork.title',
-            message: 'Neural Network',
-          })}
-          description={translate({
-            id: 'home.neuralnetwork.description',
-            message:
-              'Draw digits 0~9 below, and the neural network will recognize your handwriting in real-time',
-          })}
-        />
-
-        <div className="flex justify-center">
-          <NeuralNetworkInteractive instant={false} />
-        </div>
-      </div>
+      <SectionHeader
+        title={translate({
+          id: 'home.neuralnetwork.title',
+          message: 'Neural Network',
+        })}
+        description={translate({
+          id: 'home.neuralnetwork.description',
+          message:
+            'Draw digits 0~9 below, and the neural network will recognize your handwriting in real-time',
+        })}
+      />
+      <NeuralNetworkInteractive instant={false} />
     </SectionMain>
   );
 }
