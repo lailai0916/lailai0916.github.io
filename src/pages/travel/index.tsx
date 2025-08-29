ad4ea87699c95733f09715d71f0e0f62fb9d3ea7
lailai0916 <lailai0x394@gmail.com>
2025-08-29 13:59:19 +0800

    Update index.tsx

--- src/pages/travel/index.tsx
+++ src/pages/travel/index.tsx
@@ -8,7 +8,7 @@ import {
   PageFooter,
 } from '@site/src/components/laikit/page';
 import DataCard from '@site/src/components/laikit/widget/DataCard';
-import TravelTimeline from '@site/src/pages/travel/_components/Timeline';
+import TravelTimeline from '@site/src/pages/travel/_components/Timeline1';
 import TravelMap from '@site/src/pages/travel/_components/Map';
 import { translate } from '@docusaurus/Translate';
 import { TRAVEL_LIST } from '@site/src/data/travel';
