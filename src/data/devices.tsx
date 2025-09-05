e88b3973837a9e0a5e5803803c8000527051dde7
lailai0916 <lailai0x394@gmail.com>
2025-09-05 11:26:06 +0800

    upd

--- src/data/devices.tsx
+++ src/data/devices.tsx
@@ -6,30 +6,30 @@ export interface DeviceItem {
 export const DEVICE_LIST: DeviceItem[] = [
   {
     title: 'MacBook Pro (M3 Max)',
-    icon: '/img/device/macbook.svg',
+    icon: '/img/devices/macbook.svg',
   },
   {
     title: 'iPad Pro (M1)',
-    icon: '/img/device/ipad.svg',
+    icon: '/img/devices/ipad.svg',
   },
   {
     title: 'iPhone 13',
-    icon: '/img/device/iphone.svg',
+    icon: '/img/devices/iphone.svg',
   },
   {
     title: 'Apple Watch S10',
-    icon: '/img/device/applewatch.svg',
+    icon: '/img/devices/applewatch.svg',
   },
   {
     title: 'AirPods Pro 2',
-    icon: '/img/device/airpods.pro.svg',
+    icon: '/img/devices/airpods.pro.svg',
   },
   {
     title: 'AirPods Max',
-    icon: '/img/device/airpods.max.svg',
+    icon: '/img/devices/airpods.max.svg',
   },
   {
     title: 'Powerbeats Pro 2',
-    icon: '/img/device/beats.powerbeats.pro.svg',
+    icon: '/img/devices/beats.powerbeats.pro.svg',
   },
 ];
