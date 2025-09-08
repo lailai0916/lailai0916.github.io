12c1e71606495337e5f07389260e470650ffb03f
lailai0916 <lailai0x394@gmail.com>
2025-09-08 10:43:32 +0800

    Update changelog.tsx

--- src/data/changelog.tsx
+++ src/data/changelog.tsx
@@ -10,14 +10,10 @@ export interface ChangelogItem {
 
 export const CHANGELOG_LIST: ChangelogItem[] = [
   {
-    date: '2024-10-17',
+    date: '2025-09-05',
     type: 'added',
-    content: '网站建立',
-  },
-  {
-    date: '2025-08-13',
-    type: 'added',
-    content: 'KaTeX 复制优化插件：copy-tex',
+    content:
+      '接入搜索引擎收录平台：[Google Search Console](https://search.google.com/search-console/welcome)、[Bing Webmaster Tools](https://www.bing.com/webmasters)、[百度搜索资源平台](https://ziyuan.baidu.com/linksubmit/url)',
   },
   {
     date: '2025-09-01',
@@ -25,10 +21,24 @@ export const CHANGELOG_LIST: ChangelogItem[] = [
     content: '示例告示框组件：`example`',
   },
   {
-    date: '2025-09-05',
+    date: '2025-08-13',
     type: 'added',
-    content:
-      '接入搜索引擎收录平台：[Google Search Console](https://search.google.com/search-console/welcome)、[Bing Webmaster Tools](https://www.bing.com/webmasters)、[百度搜索资源平台](https://ziyuan.baidu.com/linksubmit/url)',
+    content: 'KaTeX 复制优化插件：copy-tex',
+  },
+  {
+    date: '2025-06-23',
+    type: 'changed',
+    content: '默认语言：English',
+  },
+  {
+    date: '2025-06-23',
+    type: 'added',
+    content: '国际化支持：English',
+  },
+  {
+    date: '2024-10-17',
+    type: 'added',
+    content: '网站建立',
   },
 ];
 
