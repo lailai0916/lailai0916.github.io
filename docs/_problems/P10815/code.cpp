6155ec8d10ac955d29c2e8bc3dbcff94a0c510ca
lailai0916 <lailai0x394@gmail.com>
2025-09-19 09:54:20 +0800

    Update code.cpp

--- docs/_problems/P10815/code.cpp
+++ docs/_problems/P10815/code.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int read()
+using ll=long long;
+ll read()
 {
-	int x=0,f=1;char c=getchar_unlocked();
+	ll x=0,f=1;char c=getchar_unlocked();
 	while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar_unlocked();}
 	while(c>='0'&&c<='9')x=x*10+c-48,c=getchar_unlocked();
 	return x*f;
 }
-void write(int x)
+void write(ll x)
 {
 	if(x<0)putchar('-'),x=-x;
 	if(x>9)write(x/10);
@@ -17,10 +18,10 @@ void write(int x)
 int main()
 {
     int n=read();
-    int ans=0;
+    ll ans=0;
     for(int i=1;i<=n;i++)
     {
-    	int x=read();
+    	ll x=read();
     	ans+=x;
     }
     write(ans);
