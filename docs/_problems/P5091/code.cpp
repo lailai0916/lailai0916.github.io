228f20842988950af706ef851ba295a017aca8c6
lailai0916 <lailai0x394@gmail.com>
2025-09-19 13:31:50 +0800

    Update code.cpp

--- docs/_problems/P5091/code.cpp
+++ docs/_problems/P5091/code.cpp
@@ -8,8 +8,8 @@ ll phi(ll n)
 	ll res=n;
 	for(ll i=2;i*i<=n;i++)
 	{
-	    if(n%i==0)
-	    {
+		if(n%i==0)
+		{
 			res=res/i*(i-1);
 			while(n%i==0)n/=i;
 		}
@@ -17,36 +17,36 @@ ll phi(ll n)
 	if(n>1)res=res/n*(n-1);
 	return res;
 }
-ll Pow(ll a,ll b,ll m)
+ll Pow(ll a,ll b,ll mod)
 {
-	a%=m;
+	a%=mod;
 	ll res=1;
 	while(b)
 	{
-		if(b&1)res=res*a%m;
-		a=a*a%m;
+		if(b&1)res=res*a%mod;
+		a=a*a%mod;
 		b>>=1;
 	}
 	return res;
 }
-ll Mod(string s,ll m)
+ll Mod(string s,ll mod)
 {
-    ll res=0,f=0;
-    for(auto c:s)
-    {
-        res=res*10+(c-'0');
-        if(res>=m)f=1;
-        res%=m;
-    }
-    return res+m*f;
+	ll res=0,f=0;
+	for(auto c:s)
+	{
+		res=res*10+(c-'0');
+		if(res>=mod)f=1;
+		res%=mod;
+	}
+	return res+mod*f;
 }
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-	ll a,m;
+	ll a,mod;
 	string b;
-	cin>>a>>m>>b;
-	cout<<Pow(a,Mod(b,phi(m)),m)<<'\n';
+	cin>>a>>mod>>b;
+	cout<<Pow(a,Mod(b,phi(mod)),mod)<<'\n';
 	return 0;
 }
