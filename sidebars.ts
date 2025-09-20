72caa890a455f2f841f3d99a903fe7952ced98af
lailai0916 <lailai0x394@gmail.com>
2025-09-20 09:49:10 +0800

    upd

--- sidebars.ts
+++ sidebars.ts
@@ -27,7 +27,7 @@ const sidebars: SidebarsConfig = {
       },
       items: [
         'contest/contest/events',
-        'contest/contest/noi',
+        'contest/contest/noi-outline',
         'contest/contest/io',
         'contest/contest/tricks',
       ],
