d8da37945224330a8bd81f3979f80da5ddc12745
lailai0916 <lailai0x394@gmail.com>
2025-09-23 14:00:08 +0800

    upd

--- src/theme/Admonition/Types.tsx
+++ src/theme/Admonition/Types.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
 import DefaultAdmonitionTypes from '@theme-original/Admonition/Types';
-import Translate from '@docusaurus/Translate';
 
 function ExampleAdmonition(props: any) {
-  const Info = (DefaultAdmonitionTypes as any).info;
-  return (
-    <Info
-      {...props}
-      type="info"
-      title={
-        props.title ?? (
-          <Translate
-            id="theme.admonition.example"
-            description="The default label used for the Example admonition (:::example)"
-          >
-            Example
-          </Translate>
-        )
-      }
-    />
-  );
+  const Info = DefaultAdmonitionTypes.info;
+  return <Info {...props} type="info" title={props.title ?? 'Example'} />;
 }
 
 const AdmonitionTypes = {
