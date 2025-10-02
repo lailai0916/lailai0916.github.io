b1a82d0f212642f438cd40b811068546ca0b0d0f
lailai0916 <lailai0x394@gmail.com>
2025-10-02 22:19:33 +0800

    Update index.tsx

--- src/components/Solution/index.tsx
+++ src/components/Solution/index.tsx
@@ -2,23 +2,30 @@ import React from 'react';
 import Link from '@docusaurus/Link';
 
 export default function Solution({ pid, aid }: { pid: string; aid: string }) {
+  const [color, setColor] = React.useState('');
+  React.useEffect(() => {
+    const value = getComputedStyle(document.documentElement)
+      .getPropertyValue('--ifm-color-primary')
+      .trim();
+    setColor(value.slice(1));
+  }, []);
   const pidEscaped = pid.replace(/_/g, '__');
 
   return (
     <>
       <Link to={`https://www.luogu.com.cn/problem/${pid}`}>
         <img
-          src={`https://img.shields.io/badge/Luogu-${pidEscaped}-blue?style=for-the-badge&logo=codeforces`}
+          src={`https://img.shields.io/badge/Luogu-${pidEscaped}-${color}?style=for-the-badge&logo=codeforces`}
         />
       </Link>{' '}
       <Link to={`https://www.luogu.com.cn/article/${aid}`}>
         <img
-          src={`https://img.shields.io/badge/Luogu-Solution-blue?style=for-the-badge&logo=markdown`}
+          src={`https://img.shields.io/badge/Luogu-Solution-${color}?style=for-the-badge&logo=markdown`}
         />
       </Link>{' '}
       <Link to={`https://lailai.one/blog/solution/${pid}`}>
         <img
-          src={`https://img.shields.io/badge/Blog-Solution-blue?style=for-the-badge&logo=markdown`}
+          src={`https://img.shields.io/badge/Blog-Solution-${color}?style=for-the-badge&logo=markdown`}
         />
       </Link>
     </>
