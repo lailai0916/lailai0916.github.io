de7195ae3faf1c423e098ed64f71997723b4c5f6
lailai0916 <lailai0x394@gmail.com>
2025-10-05 12:29:57 +0800

    Update code.cpp

--- docs/_problems/P4980/code.cpp
+++ docs/_problems/P4980/code.cpp
@@ -19,21 +19,16 @@ ll Pow(ll a,ll b)
 }
 ll phi(ll n)
 {
-	ll res=1;
-	for(int i=2;i*i<=n;i++)
+	ll res=n;
+	for(ll i=2;i*i<=n;i++)
 	{
 		if(n%i==0)
 		{
-			n/=i;
-			res=res*(i-1);
-			while(n%i==0)
-			{
-				n/=i;
-				res*=i;
-			}
+			res=res/i*(i-1);
+			while(n%i==0)n/=i;
 		}
 	}
-	if(n>1)res*=(n-1);
+	if(n>1)res=res/n*(n-1);
 	return res;
 }
 ll polya(ll n,ll m)
