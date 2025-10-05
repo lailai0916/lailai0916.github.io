11f6b4c11e30a33d3fa2b5ac5c3c1d668ef5878d
lailai0916 <lailai0x394@gmail.com>
2025-10-05 22:53:55 +0800

    Update code.cpp

--- docs/_problems/P1255/code.cpp
+++ docs/_problems/P1255/code.cpp
@@ -2,26 +2,27 @@
 using namespace std;
 
 const int N=5005;
-int a[N][N];
+int a[2][N]={{1},{2}};
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	int n;
 	cin>>n;
-	a[1][1]=1;
-	a[2][1]=2;
 	int len=1;
-	for(int i=3;i<=n;i++)
+	for(int i=2;i<n;i++)
 	{
-		for(int j=1;j<=len;j++)
+		for(int j=0;j<len;j++)
 		{
-			a[i][j]+=a[i-1][j]+a[i-2][j];
-			a[i][j+1]+=a[i][j]/10;
-			a[i][j]%=10;
+			a[i&1][j]+=a[i&1^1][j];
 		}
-		if(a[i][len+1]>0)len++;
+		for(int j=0;j<len;j++)
+		{
+			a[i&1][j+1]+=a[i&1][j]/10;
+			a[i&1][j]%=10;
+		}
+		if(a[i&1][len])len++;
 	}
-	for(int i=len;i>=1;i--)cout<<a[n][i];
+	for(int i=len-1;i>=0;i--)cout<<a[n&1^1][i];
 	return 0;
 }
