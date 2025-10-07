e1492763ce5f4f905d6737310967606f0d79e199
lailai0916 <lailai0x394@gmail.com>
2025-10-07 23:31:19 +0800

    Update 树链剖分.cpp

--- docs/_problems/P3379/树链剖分.cpp
+++ docs/_problems/P3379/树链剖分.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 const int N=500005;
 vector<int> G[N];
-int fa[N],son[N],siz[N],dep[N];
-int top[N],dfn[N],rnk[N],out[N];
+int fa[N],son[N],siz[N],dep[N],top[N];
 int cnt=0;
 void dfs1(int u)
 {
@@ -22,15 +21,12 @@ void dfs1(int u)
 void dfs2(int u,int t)
 {
 	top[u]=t;
-	dfn[u]=++cnt;
-	rnk[cnt]=u;
 	if(son[u])dfs2(son[u],t);
 	for(auto v:G[u])
 	{
 		if(v==fa[u]||v==son[u])continue;
 		dfs2(v,v);
 	}
-	out[u]=cnt;
 }
 int lca(int u,int v)
 {
