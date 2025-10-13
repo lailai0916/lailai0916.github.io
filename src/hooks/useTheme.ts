4e46455aa1626185e7607cf210736130d24024dd
lailai0916 <lailai0x394@gmail.com>
2025-10-13 12:32:21 +0800

    Update useTheme.ts

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,7 +1,7 @@
 import { createContext, useContext } from 'react';
 
 export const ThemeContext = createContext({
-  isNewLayout: false,
+  isNewLayout: true,
   setIsNewLayout: (_: boolean) => {},
 });
 
