2ac4db930218cc3b610bc3df91bfa3f13b19b7ad
lailai0916 <lailai0x394@gmail.com>
2025-10-19 16:44:01 +0800

    Update index.tsx

--- src/components/laikit/page/index.tsx
+++ src/components/laikit/page/index.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
 import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import Layout from '@theme/Layout';
+import type { Props } from '@theme/Layout';
 import { useDebugMode } from '@site/src/hooks/useDebugMode';
 import styles from './styles.module.css';
 
-export function DebugLayout({
-  title,
-  description,
-  children,
-}: {
-  title: string;
-  description: string;
-  children: React.ReactNode;
-}) {
+export function DebugLayout(props: Props) {
   const debugMode = useDebugMode();
   return (
-    <Layout title={title} description={description}>
-      <div className={clsx(debugMode && styles.debug)}>{children}</div>
-    </Layout>
+    <div className={clsx(debugMode && styles.debug)}>
+      <Layout {...props} />
+    </div>
   );
 }
 
