44625c6a1aee8582a86b51247660c3a94ab2d3bc
lailai0916 <lailai0x394@gmail.com>
2025-11-12 02:17:57 +0800

    Update code.cpp

--- docs/_problems/P5656/code.cpp
+++ docs/_problems/P5656/code.cpp
@@ -21,10 +21,9 @@ int main()
 		if(c%d){cout<<-1<<'\n';continue;}
 		x*=c/d;y*=c/d;
 		ll x1=b/d,y1=a/d;
-		ll x_min=(x%x1+x1-1)%x1+1,y_min=(y%y1+y1-1)%y1+1;
-		ll x_max=(c-b*y_min)/a,y_max=(c-a*x_min)/b;
-		if(y_max<=0)cout<<x_min<<' '<<y_min<<'\n';
-		else cout<<(x_max-x_min)/x1+1<<' '<<x_min<<' '<<y_min<<' '<<x_max<<' '<<y_max<<'\n';
+		ll x_min=(x%x1+x1-1)%x1+1,y_min=(y%y1+y1-1)%y1+1,x_max=(c-b*y_min)/a,y_max=(c-a*x_min)/b;
+		if(y_max>0)cout<<(x_max-x_min)/x1+1<<' '<<x_min<<' '<<y_min<<' '<<x_max<<' '<<y_max<<'\n';
+		else cout<<x_min<<' '<<y_min<<'\n';
 	}
 	return 0;
 }
