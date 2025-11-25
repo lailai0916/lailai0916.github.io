2aee872c53716cf1b34809309975e9c9cfa040ed
lailai0916 <lailai0x394@gmail.com>
2025-11-25 16:44:42 +0800

    upd

--- docs/_problems/P1939/code.cpp
+++ docs/_problems/P1939/code.cpp
@@ -7,6 +7,7 @@ const int N=3;
 struct Mat
 {
 	ll a[N][N];
+	Mat(){memset(a,0,sizeof a);}
 	Mat operator*(const Mat &rhs) const
 	{
 		Mat res;
@@ -14,7 +15,6 @@ struct Mat
 		{
 			for(int j=0;j<N;j++)
 			{
-				res.a[i][j]=0;
 				for(int k=0;k<N;k++)
 				{
 					res.a[i][j]=(res.a[i][j]+a[i][k]*rhs.a[k][j])%mod;
@@ -23,16 +23,10 @@ struct Mat
 		}
 		return res;
 	}
-	Mat operator^(ll rhs)
+	Mat operator^(ll rhs) const
 	{
 		Mat res,tmp=*this;
-		for(int i=0;i<N;i++)
-		{
-			for(int j=0;j<N;j++)
-			{
-				res.a[i][j]=i==j;
-			}
-		}
+		for(int i=0;i<N;i++)res.a[i][i]=1;
 		while(rhs)
 		{
 			if(rhs&1)res=res*tmp;
@@ -52,8 +46,11 @@ int main()
 	{
 		ll n;
 		cin>>n;
-		Mat mat={{{1,0,1},{1,0,0},{0,1,0}}};
-		mat=mat^(n-1);
+		Mat mat;
+		mat.a[0][0]=1;mat.a[0][1]=0;mat.a[0][2]=1;
+		mat.a[1][0]=1;mat.a[1][1]=0;mat.a[1][2]=0;
+		mat.a[2][0]=0;mat.a[2][1]=1;mat.a[2][2]=0;
+		mat=mat^n-1;
 		cout<<mat.a[0][0]<<'\n';
 	}
 	return 0;
--- docs/_problems/P1962/code.cpp
+++ docs/_problems/P1962/code.cpp
@@ -7,51 +7,45 @@ const int N=2;
 struct Mat
 {
 	ll a[N][N];
-	Mat operator*(const Mat &b) const
+	Mat(){memset(a,0,sizeof a);}
+	Mat operator*(const Mat &rhs) const
 	{
 		Mat res;
 		for(int i=0;i<N;i++)
 		{
 			for(int j=0;j<N;j++)
 			{
-				res.a[i][j]=0;
 				for(int k=0;k<N;k++)
 				{
-					res.a[i][j]=(res.a[i][j]+a[i][k]*b.a[k][j])%mod;
+					res.a[i][j]=(res.a[i][j]+a[i][k]*rhs.a[k][j])%mod;
 				}
 			}
 		}
 		return res;
 	}
-	Mat operator^(ll b)
+	Mat operator^(ll rhs) const
 	{
 		Mat res,tmp=*this;
-		for(int i=0;i<N;i++)
+		for(int i=0;i<N;i++)res.a[i][i]=1;
+		while(rhs)
 		{
-			for(int j=0;j<N;j++)
-			{
-				res.a[i][j]=(i==j);
-			}
-		}
-		while(b)
-		{
-			if(b&1)res=res*tmp;
+			if(rhs&1)res=res*tmp;
 			tmp=tmp*tmp;
-			b>>=1;
+			rhs>>=1;
 		}
 		return res;
-	}
-	void operator^=(ll b)
-	{
-		*this=*this^b;
 	}
 };
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 	ll n;
 	cin>>n;
-	Mat ans={{{1,1},{1,0}}};
-	ans^=n-1;
-	cout<<ans.a[0][0]<<'\n';
+	Mat mat;
+	mat.a[0][0]=1;mat.a[0][1]=1;
+	mat.a[1][0]=1;mat.a[1][1]=0;
+	mat=mat^n-1;
+	cout<<mat.a[0][0]<<'\n';
 	return 0;
 }
