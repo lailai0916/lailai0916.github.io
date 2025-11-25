8f3e31cb1aa284f830a383523e4e87af3d2fd943
lailai0916 <lailai0x394@gmail.com>
2025-11-25 16:53:39 +0800

    upd

--- docs/_problems/P1349/code.cpp
+++ docs/_problems/P1349/code.cpp
@@ -1,52 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll=long long;
 const int N=2;
-long long p,q,a1,a2,n,m;
+ll a1,a2,mod;
 struct Mat
 {
-	long long a[5][5];
-	Mat operator*(const Mat &x) const
+	ll a[N][N];
+	Mat(){memset(a,0,sizeof a);}
+	Mat operator*(const Mat &rhs) const
 	{
 		Mat res;
-		for(int i=1;i<=N;i++)
+		for(int i=0;i<N;i++)
 		{
-			for(int j=1;j<=N;j++)
+			for(int j=0;j<N;j++)
 			{
-				res.a[i][j]=0;
-				for(int k=1;k<=N;k++)
+				for(int k=0;k<N;k++)
 				{
-					res.a[i][j]=(res.a[i][j]+a[i][k]*x.a[k][j])%m;
+					res.a[i][j]=(res.a[i][j]+a[i][k]*rhs.a[k][j])%mod;
 				}
 			}
 		}
 		return res;
 	}
-};
-Mat Pow(Mat x,long long k)
-{
-	Mat res;
-	res.a[1][1]=a2;res.a[1][2]=a1;
-	res.a[2][1]=0;res.a[2][2]=0;
-	while(k)
+	Mat operator^(ll rhs) const
 	{
-		if(k&1)res=res*x;
-		x=x*x;
-		k>>=1;
+		Mat res,tmp=*this;
+		res.a[0][0]=a2;res.a[0][1]=a1;
+		res.a[1][0]=0;res.a[1][1]=0;
+		while(rhs)
+		{
+			if(rhs&1)res=res*tmp;
+			tmp=tmp*tmp;
+			rhs>>=1;
+		}
+		return res;
 	}
-	return res;
-}
+};
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-	cin>>p>>q>>a1>>a2>>n>>m;
+	ll p,q,n;
+	cin>>p>>q>>a1>>a2>>n>>mod;
 	if(n==1){cout<<a1<<'\n';return 0;}
 	if(n==2){cout<<a2<<'\n';return 0;}
-	Mat ans;
-	ans.a[1][1]=p;ans.a[1][2]=1;
-	ans.a[2][1]=q;ans.a[2][2]=0;
-	ans=Pow(ans,n-2);
-	cout<<ans.a[1][1]%m<<'\n';
+	Mat mat;
+	mat.a[0][0]=p;mat.a[0][1]=1;
+	mat.a[1][0]=q;mat.a[1][1]=0;
+	mat=mat^n-2;
+	cout<<mat.a[0][0]%mod<<'\n';
 	return 0;
 }
