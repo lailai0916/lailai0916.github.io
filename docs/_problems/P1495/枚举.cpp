6e17304e75e9e4fca45e7eae4662fa22250d71be
lailai0916 <lailai0x394@gmail.com>
2025-12-09 01:04:11 +0800

    Update 枚举.cpp

--- docs/_problems/P1495/枚举.cpp
+++ docs/_problems/P1495/枚举.cpp
@@ -11,8 +11,8 @@ int main()
 	int n;
 	cin>>n;
 	for(int i=1;i<=n;i++)cin>>a[i]>>b[i];
-	ll ans=b[1],sum=a[1];
-	for(int i=2;i<=n;i++)
+	ll ans=0,sum=1;
+	for(int i=1;i<=n;i++)
 	{
 		while(ans%a[i]!=b[i])ans+=sum;
 		sum*=a[i];
