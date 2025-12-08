23c06dc85e04cbeadc1ea69e8001caf53dca3cbb
lailai0916 <lailai0x394@gmail.com>
2025-12-09 01:28:13 +0800

    Update 枚举.cpp

--- docs/_problems/P1495/枚举.cpp
+++ docs/_problems/P1495/枚举.cpp
@@ -2,20 +2,19 @@
 using namespace std;
 
 using ll=long long;
-const int N=15;
-ll a[N],b[N];
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	int n;
 	cin>>n;
-	for(int i=1;i<=n;i++)cin>>a[i]>>b[i];
 	ll ans=0,sum=1;
 	for(int i=1;i<=n;i++)
 	{
-		while(ans%a[i]!=b[i])ans+=sum;
-		sum*=a[i];
+		ll a,b;
+		cin>>a>>b;
+		while(ans%a!=b)ans+=sum;
+		sum*=a;
 	}
 	cout<<ans<<'\n';
 	return 0;
