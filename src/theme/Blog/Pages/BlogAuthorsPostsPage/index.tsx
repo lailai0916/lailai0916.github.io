dab437aca52a73d49e9319893ffc4c35f2fcfa73
lailai0916 <lailai0x394@gmail.com>
2025-12-23 22:59:10 +0800

    Update index.tsx

--- src/theme/Blog/Pages/BlogAuthorsPostsPage/index.tsx
+++ src/theme/Blog/Pages/BlogAuthorsPostsPage/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { type ReactNode } from 'react';
 import type { Props } from '@theme/Blog/Pages/BlogAuthorsPostsPage';
 import PostsListLayout from '../../../BlogShared/PostsListLayout';
 
-export default function BlogAuthorsPostsPage(props: Props) {
+export default function BlogAuthorsPostsPage(props: Props): ReactNode {
   const { author, items, listMetadata } = props;
+
   return (
     <PostsListLayout
       title={author.name}
