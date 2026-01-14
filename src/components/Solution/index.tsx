fa3aae74f0a777ee1eb09d2215de4ff6ba3cf3e0
lailai0916 <lailai0x394@gmail.com>
2026-01-15 00:47:11 +0800

    upd

--- src/components/Solution/index.tsx
+++ src/components/Solution/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
+import { translate } from '@docusaurus/Translate';
+
+const luogu = translate({ id: 'components.solution.luogu', message: 'Luogu' });
+const blog = translate({ id: 'components.solution.blog', message: 'Blog' });
+const solution = translate({
+  id: 'components.solution.solution',
+  message: 'Solution',
+});
 
 export default function Solution({ pid, aid }: { pid: string; aid: string }) {
   const [color, setColor] = React.useState('');
@@ -15,17 +23,17 @@ export default function Solution({ pid, aid }: { pid: string; aid: string }) {
     <>
       <Link href={`https://www.luogu.com.cn/problem/${pid}`}>
         <img
-          src={`https://img.shields.io/badge/Luogu-${pidEscaped}-${color}?style=for-the-badge&logo=luogu`}
+          src={`https://img.shields.io/badge/${luogu}-${pidEscaped}-${color}?style=for-the-badge&logo=luogu`}
         />
       </Link>{' '}
       <Link href={`https://www.luogu.com.cn/article/${aid}`}>
         <img
-          src={`https://img.shields.io/badge/Luogu-Solution-${color}?style=for-the-badge&logo=luogu`}
+          src={`https://img.shields.io/badge/${luogu}-${solution}-${color}?style=for-the-badge&logo=luogu`}
         />
       </Link>{' '}
       <Link href={`https://lailai.one/blog/solution/${pid}`}>
         <img
-          src={`https://img.shields.io/badge/Blog-Solution-${color}?style=for-the-badge&logo=markdown`}
+          src={`https://img.shields.io/badge/${blog}-${solution}-${color}?style=for-the-badge&logo=markdown`}
         />
       </Link>
     </>
