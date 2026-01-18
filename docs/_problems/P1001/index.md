72f7454410d253da1a448ab61db76e3dd701f8e6
lailai0916 <lailai0x394@gmail.com>
2026-01-18 12:47:41 +0800

    upd

--- docs/_problems/P1001/index.md
+++ docs/_problems/P1001/index.md
@@ -1,5 +1,6 @@
-:::info[[洛谷 P1001 A+B Problem](https://www.luogu.com.cn/problem/P1001)]
+---
+title: '洛谷 P1001 A+B Problem'
+link: 'https://www.luogu.com.cn/problem/P1001'
+---
 
 输入两个整数 $a,b$，输出它们的和。（$|a|,|b| \le {10}^9$）
-
-:::
--- src/components/Problem/index.tsx
+++ src/components/Problem/index.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo } from 'react';
+import Link from '@docusaurus/Link';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 import Details from '@theme/Details';
@@ -13,7 +14,7 @@ const ctx = require.context(
   /\.cpp$/
 );
 
-export function GetCode({ id }: { id: string }) {
+function GetCode({ id }: { id: string }) {
   const formatTitle = (p: string) =>
     p
       .slice(p.lastIndexOf('/') + 1)
@@ -31,42 +32,42 @@ export function GetCode({ id }: { id: string }) {
       .sort((a, b) => a.title.localeCompare(b.title));
   }, [id]);
 
+  if (codes.length === 0) return <></>;
+
   return (
-    <>
-      {codes.length > 0 && (
-        <Details
-          summary={translate(
-            {
-              id: 'components.problem.code',
-              message: 'Code ({num})',
-            },
-            { num: codes.length }
-          )}
-        >
-          {codes.length === 1 ? (
-            <CodeBlock language="cpp">{codes[0].code}</CodeBlock>
-          ) : (
-            <Tabs>
-              {codes.map(({ title, code }) => (
-                <TabItem key={title} value={title}>
-                  <CodeBlock language="cpp">{code}</CodeBlock>
-                </TabItem>
-              ))}
-            </Tabs>
-          )}
-        </Details>
+    <Details
+      summary={translate(
+        {
+          id: 'components.problem.code',
+          message: 'Code ({num})',
+        },
+        { num: codes.length }
       )}
-    </>
+    >
+      {codes.length === 1 ? (
+        <CodeBlock language="cpp">{codes[0].code}</CodeBlock>
+      ) : (
+        <Tabs>
+          {codes.map(({ title, code }) => (
+            <TabItem key={title} value={title}>
+              <CodeBlock language="cpp">{code}</CodeBlock>
+            </TabItem>
+          ))}
+        </Tabs>
+      )}
+    </Details>
   );
 }
 
-export function GetSolution({ id }: { id: string }) {
-  let SOL: React.ComponentType;
+function GetSolution({ id }: { id: string }) {
+  let mdxModule: any;
   try {
-    SOL = require(`@site/blog/solution/${id}.md`).default;
+    mdxModule = require(`@site/blog/solution/${id}.md`);
   } catch {
     return <></>;
   }
+  const { default: SOL } = mdxModule;
+
   return (
     <Details
       summary={translate({
@@ -80,29 +81,33 @@ export function GetSolution({ id }: { id: string }) {
 }
 
 export default function Problem({ id }: { id: string }) {
-  let MDX: React.ComponentType;
+  let mdxModule: any;
   try {
-    MDX = require(`@site/docs/_problems/${id}/index.md`).default;
+    mdxModule = require(`@site/docs/_problems/${id}/index.md`);
   } catch {
     return (
       <Admonition type="warning">
         {translate(
           {
             id: 'components.problem.error',
-            message:
-              'Unable to load problem {id}: please check if the file exists.',
+            message: 'Unable to load problem {id}.',
           },
           { id }
         )}
       </Admonition>
     );
   }
+  const { default: MDX, frontMatter } = mdxModule;
+  const { title = id, link } = frontMatter;
 
   return (
-    <>
+    <Admonition
+      type="example"
+      title={link ? <Link href={link}>{title}</Link> : title}
+    >
       {React.createElement(MDX)}
       <GetSolution id={id} />
       <GetCode id={id} />
-    </>
+    </Admonition>
   );
 }
