78e709fc74fa8befec6cededdfc371e5b8aef904
lailai0916 <lailai0x394@gmail.com>
2026-01-21 00:00:56 +0800

    Create index.tsx

---
+++ src/pages/moments/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BlogScaffold from '@site/src/theme/BlogShared/Scaffold';
+import { Card } from '@site/src/theme/BlogShared/Components';
+
+export default function moments() {
+  return (
+    <BlogScaffold title="Moments" description="lailai's Moments">
+      <Card>{'test'}</Card>
+    </BlogScaffold>
+  );
+}
