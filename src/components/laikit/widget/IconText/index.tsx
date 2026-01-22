4f386b8a5c15febe4936967ef65ef1c4b1373ed5
lailai0916 <lailai0x394@gmail.com>
2026-01-22 23:27:52 +0800

    upd

--- src/components/laikit/widget/IconText/index.tsx
+++ src/components/laikit/widget/IconText/index.tsx
@@ -4,20 +4,22 @@ import styles from './styles.module.css';
 interface IconTextProps {
   icon: string;
   children: React.ReactNode;
+  size?: string;
   colorMode?: 'theme' | 'monochrome';
 }
 
 export default function IconText({
   icon,
   children,
+  size = '1.25em',
   colorMode = 'theme',
 }: IconTextProps) {
   return (
     <span className={styles.iconText}>
       <Icon
         icon={icon}
-        width="1.25em"
-        height="1.25em"
+        width={size}
+        height={size}
         className={colorMode === 'theme' && styles.iconTheme}
       />
       <span>{children}</span>
--- src/pages/moments/index.tsx
+++ src/pages/moments/index.tsx
@@ -3,16 +3,18 @@ import BlogScaffold from '@site/src/theme/BlogShared/Scaffold';
 import { Card } from '@site/src/theme/BlogShared/Components';
 import { MOMENT_LIST } from '@site/src/data/moments';
 import styles from './styles.module.css';
+import IconText from '@site/src/components/laikit/widget/IconText';
+
+const TITLE = 'Moments';
+const DESCRIPTION = 'Share life, anytime, anywhere';
 
 export default function moments() {
   return (
-    <BlogScaffold title="Moments" description="lailai's Moments">
+    <BlogScaffold title={TITLE} description={DESCRIPTION}>
       <Card>
         <div className={styles.headerSection}>
-          <div>
-            <h1 className={styles.title}>Moments</h1>
-            <p className={styles.description}>Share life, anytime, anywhere</p>
-          </div>
+          <h1 className={styles.title}>{TITLE}</h1>
+          <p className={styles.description}>{DESCRIPTION}</p>
         </div>
       </Card>
       {MOMENT_LIST.map((moment) => (
@@ -26,13 +28,15 @@ export default function moments() {
             </div>
           )}
           <hr className={styles.hr} />
-          <span>
-            {new Date(moment.date).toLocaleDateString('en-US', {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
-            })}
-          </span>
+          <div className={styles.momentMeta}>
+            <IconText icon="lucide:calendar" colorMode="monochrome">
+              {new Date(moment.date).toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+              })}
+            </IconText>
+          </div>
         </Card>
       ))}
     </BlogScaffold>
