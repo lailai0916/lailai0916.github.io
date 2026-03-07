bb13f6c83aa7aefb9d6f9e707d79543f56cab114
lailai0916 <lailai0x394@gmail.com>
2026-03-07 19:00:40 +0800

    Update moments.tsx

--- src/data/moments.tsx
+++ src/data/moments.tsx
@@ -6,6 +6,11 @@ export interface MomentItem {
 
 export const MOMENT_LIST: MomentItem[] = [
   {
+    date: '2026-03-07T14:30',
+    content:
+      '时过境迁，小 B 回到了他梦寐以求，却又折戟沉沙的省选赛场。但他关于算法竞赛的记忆还有多少呢？其中又有多少最为珍贵的记忆值得去珍惜呢？小 B 是一个对算法竞赛充满热情，乐于探索的人。而对他来说，最珍贵的记忆便是学习算法时对其进行各种修改、实验，尝试得到一些新成果的日子吧。',
+  },
+  {
     date: '2026-01-22T10:30',
     content: 'Hello, World!',
   },
