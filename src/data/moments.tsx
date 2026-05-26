01a9eca5478b16b28a62ef421a3c7ccbe8e6470b
lailai0916 <lailai0x394@gmail.com>
2026-05-26 15:37:29 +0800

    Update moments.tsx

--- src/data/moments.tsx
+++ src/data/moments.tsx
@@ -13,14 +13,6 @@ interface MomentItem {
 
 const STATIC_MOMENTS: MomentItem[] = [
   {
-    date: '2026-02-21T19:49',
-    share: {
-      url: 'https://www.bilibili.com/video/BV1R4fnBhELd',
-      title: 'LGS Paintboard 2026 延时动画',
-      description: 'lailai0916',
-    },
-  },
-  {
     date: '2026-03-30T18:30',
     content:
       'bilibili LV6! <a href="https://space.bilibili.com/1796257032">@lailai0916</a>',
@@ -31,6 +23,15 @@ const STATIC_MOMENTS: MomentItem[] = [
       '时过境迁，小 B 回到了他梦寐以求，却又折戟沉沙的省选赛场。但他关于算法竞赛的记忆还有多少呢？其中又有多少最为珍贵的记忆值得去珍惜呢？小 B 是一个对算法竞赛充满热情，乐于探索的人。而对他来说，最珍贵的记忆便是学习算法时对其进行各种修改、实验，尝试得到一些新成果的日子吧。',
     event: '联合省选 2026',
     location: '杭州师范大学（仓前校区）',
+  },
+  {
+    date: '2026-02-21T19:49',
+    event: 'LGS Paintboard 2026',
+    share: {
+      url: 'https://www.bilibili.com/video/BV1R4fnBhELd',
+      title: 'LGS Paintboard 2026 延时动画',
+      description: 'lailai0916',
+    },
   },
   {
     date: '2026-01-22T10:30',
