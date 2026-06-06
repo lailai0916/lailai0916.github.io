da95b19cf19311ec8fe2b0bb8ff6968a53d3f173
lailai0916 <lailai0x394@gmail.com>
2026-06-06 22:26:18 +0800

    upd

--- src/data/moments.tsx
+++ src/data/moments.tsx
@@ -13,7 +13,7 @@ interface MomentItem {
 
 const STATIC_MOMENTS: MomentItem[] = [
   {
-    date: '2026-05-31T15:55',
+    date: '2026-05-31T15:55:00',
     share: {
       url: 'https://www.bilibili.com/video/BV1mkVn63EYQ',
       title: '信息学奥林匹克竞赛学校排名变迁动画',
@@ -21,7 +21,7 @@ const STATIC_MOMENTS: MomentItem[] = [
     },
   },
   {
-    date: '2026-03-30T18:30',
+    date: '2026-03-30T18:30:00',
     content: 'bilibili LV6!',
     share: {
       url: 'https://space.bilibili.com/1796257032',
@@ -30,14 +30,14 @@ const STATIC_MOMENTS: MomentItem[] = [
     },
   },
   {
-    date: '2026-03-07T14:30',
+    date: '2026-03-07T14:30:00',
     content:
       '时过境迁，小 B 回到了他梦寐以求，却又折戟沉沙的省选赛场。但他关于算法竞赛的记忆还有多少呢？其中又有多少最为珍贵的记忆值得去珍惜呢？小 B 是一个对算法竞赛充满热情，乐于探索的人。而对他来说，最珍贵的记忆便是学习算法时对其进行各种修改、实验，尝试得到一些新成果的日子吧。',
     event: '联合省选 2026',
     location: '杭州师范大学（仓前校区）',
   },
   {
-    date: '2026-02-21T19:49',
+    date: '2026-02-21T19:49:00',
     event: 'LGS Paintboard 2026',
     share: {
       url: 'https://www.bilibili.com/video/BV1R4fnBhELd',
@@ -46,36 +46,36 @@ const STATIC_MOMENTS: MomentItem[] = [
     },
   },
   {
-    date: '2026-01-22T10:30',
+    date: '2026-01-22T10:30:00',
     content: 'Hello, World!',
   },
   {
-    date: '2025-12-26T23:23',
+    date: '2025-12-26T23:23:00',
     content: 'ChatGPT 2025',
     event: 'Your Year with ChatGPT',
     images: ['https://cloud.lailai.one/f/PVyuy/chatgpt-2025.png'],
   },
   {
-    date: '2025-10-17T00:00',
+    date: '2025-10-17T00:00:00',
     content: "Website's 1st Anniversary! 🎉",
   },
   {
-    date: '2025-10-16T19:50',
+    date: '2025-10-16T19:50:00',
     content: '图灵测试',
     images: ['https://cloud.lailai.one/f/YvpHA/certificate.png'],
   },
   {
-    date: '2025-10-05T19:45',
+    date: '2025-10-05T19:45:00',
     content: 'GIMPS | Lucas–Lehmer Primality Test',
     images: ['https://cloud.lailai.one/f/wrhg/gimps.png'],
   },
   {
-    date: '2025-09-16T00:00',
+    date: '2025-09-16T00:00:00',
     content: '16th Birthday! 🎉',
     images: ['https://cloud.lailai.one/f/r4HM/birthday-16.png'],
   },
   {
-    date: '2025-05-23T22:22',
+    date: '2025-05-23T22:22:00',
     content: 'Java 30th!',
   },
 ];
