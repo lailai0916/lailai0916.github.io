72ac7ed0f0b77ab4a79e47bfe52fbec573ffdc82
lailai0916 <lailai0x394@gmail.com>
2026-08-20 19:46:03 +0800

    upd

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -1,7 +1,8 @@
 import { type ReactNode } from 'react';
 import Layout from '@theme/Layout';
+import { Icon } from '@iconify/react';
 import { PageTitle, PageHeader, PageContent } from '@site/src/components/laikit/Page';
-import DataCard from '@site/src/components/laikit/DataCard';
+import Button from '@site/src/components/laikit/Button';
 import LinkCard from '@site/src/components/laikit/LinkCard';
 import { FRIEND_LIST } from '@site/src/data/friends';
 import { translate } from '@docusaurus/Translate';
@@ -19,20 +20,39 @@ const MODIFICATION = translate({
   id: 'pages.friends.modification',
   message: 'My <b>Friends</b>',
 });
+const REQUEST_LINK = translate({
+  id: 'pages.friends.requestLink',
+  message: 'Request a Link',
+});
+const REQUEST_SUBJECT = translate({
+  id: 'pages.friends.requestSubject',
+  message: 'Friend Link Request',
+});
+
+const REQUEST_TEMPLATE = `{
+  title: "lailai's Home",
+  description: 'Student & Developer',
+  href: 'https://lailai.one',
+  avatar: 'https://lailai.one/img/logo.png',
+}`;
+const REQUEST_LINK_EMAIL = `mailto:lailai0x394@gmail.com?subject=${encodeURIComponent(
+  REQUEST_SUBJECT
+)}&body=${encodeURIComponent(REQUEST_TEMPLATE)}`;
 
 export default function Friends(): ReactNode {
   return (
     <Layout title={TITLE} description={DESCRIPTION}>
       <PageHeader>
         <PageTitle title={MODIFICATION} description={DESCRIPTION} />
-        <DataCard
-          value={FRIEND_LIST.length}
-          label={translate({
-            id: 'pages.friends.datacard.label',
-            message: 'Friend|Friends',
-          })}
-          icon="lucide:users"
-        />
+        <Button
+          variant="secondary"
+          leftIcon={<Icon icon="lucide:link-2" width={16} height={16} />}
+          onClick={() => {
+            window.location.href = REQUEST_LINK_EMAIL;
+          }}
+        >
+          {REQUEST_LINK}
+        </Button>
       </PageHeader>
       <PageContent className={styles.layout}>
         {FRIEND_LIST.map((friend, index) => (
