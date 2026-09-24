63729d195d172566bddf4e71834a6b79141845a4
lailai0916 <lailai0x394@gmail.com>
2026-09-24 23:56:59 +0800

    upd

--- src/data/friends.tsx
+++ src/data/friends.tsx
@@ -1,3 +1,5 @@
+import { translate } from '@docusaurus/Translate';
+
 interface FriendItem {
   title: string;
   description?: string;
@@ -7,98 +9,203 @@ interface FriendItem {
 
 export const FRIEND_LIST: FriendItem[] = [
   {
-    title: 'Sintle',
-    description: 'Code, Learn, Share',
+    title: translate({
+      id: 'data.friends.sintle.title',
+      message: 'Sintle',
+    }),
+    description: translate({
+      id: 'data.friends.sintle.description',
+      message: 'Code, Learn, Share',
+    }),
     href: 'https://sintle.github.io',
     avatar: 'https://q1.qlogo.cn/g?b=qq&nk=3545418095&s=640',
   },
   {
-    title: 'YunLuo',
-    description: '这是个人',
+    title: translate({
+      id: 'data.friends.yunLuo.title',
+      message: 'YunLuo',
+    }),
+    description: translate({
+      id: 'data.friends.yunLuo.description',
+      message: '这是个人',
+    }),
     href: 'https://www.cnblogs.com/yunluo-qwq',
     avatar: 'https://cdn.luogu.com.cn/upload/usericon/765961.png',
   },
   {
-    title: 'Mirekintoc',
-    description: 'Zate feíam sucur ilém',
+    title: translate({
+      id: 'data.friends.mirekintoc.title',
+      message: 'Mirekintoc',
+    }),
+    description: translate({
+      id: 'data.friends.mirekintoc.description',
+      message: 'Zate feíam sucur ilém',
+    }),
     href: 'https://vochant.github.io',
     avatar: 'https://vochant.github.io/images/logo.svg',
   },
   {
-    title: 'AKorz',
-    description: 'FACE THE SIN, SAVE THE E.G.O.',
+    title: translate({
+      id: 'data.friends.akorz.title',
+      message: 'AKorz',
+    }),
+    description: translate({
+      id: 'data.friends.akorz.description',
+      message: 'FACE THE SIN, SAVE THE E.G.O.',
+    }),
     href: 'https://www.luogu.com.cn/user/716602',
     avatar: 'https://cdn.luogu.com.cn/upload/usericon/716602.png',
   },
   {
-    title: 'CEFqwq',
-    description: '代词使用她',
+    title: translate({
+      id: 'data.friends.cefqwq.title',
+      message: 'CEFqwq',
+    }),
+    description: translate({
+      id: 'data.friends.cefqwq.description',
+      message: '代词使用她',
+    }),
     href: 'https://www.luogu.com.cn/user/482610',
     avatar: 'https://cdn.luogu.com.cn/upload/usericon/482610.png',
   },
   {
-    title: 'LaDeX',
-    description: 'Ciallo～(∠・ω< )⌒★',
+    title: translate({
+      id: 'data.friends.ladex.title',
+      message: 'LaDeX',
+    }),
+    description: translate({
+      id: 'data.friends.ladex.description',
+      message: 'Ciallo～(∠・ω< )⌒★',
+    }),
     href: 'https://www.luogu.com.cn/user/431658',
     avatar: 'https://cdn.luogu.com.cn/upload/usericon/431658.png',
   },
   {
-    title: 'Dream__maker',
-    description: '一息尚存，方能涅槃而生',
+    title: translate({
+      id: 'data.friends.dreamMaker.title',
+      message: 'Dream__maker',
+    }),
+    description: translate({
+      id: 'data.friends.dreamMaker.description',
+      message: '一息尚存，方能涅槃而生',
+    }),
     href: 'https://www.cnblogs.com/jdzhzw',
     avatar: 'https://cdn.luogu.com.cn/upload/usericon/718017.png',
   },
   {
-    title: 'Jasonshan10',
+    title: translate({
+      id: 'data.friends.jasonshan10.title',
+      message: 'Jasonshan10',
+    }),
     href: 'https://www.cnblogs.com/Jasonshan10',
     avatar: 'https://q1.qlogo.cn/g?b=qq&nk=342200890&s=640',
   },
   {
-    title: 'SJZ2010',
-    description: '初心不改，求索依然。',
+    title: translate({
+      id: 'data.friends.sjz2010.title',
+      message: 'SJZ2010',
+    }),
+    description: translate({
+      id: 'data.friends.sjz2010.description',
+      message: '初心不改，求索依然。',
+    }),
     href: 'https://www.cnblogs.com/decimal10',
     avatar: 'https://cdn.luogu.com.cn/upload/usericon/809729.png',
   },
   {
-    title: 'Castamere',
-    description: '代码美学',
+    title: translate({
+      id: 'data.friends.castamere.title',
+      message: 'Castamere',
+    }),
+    description: translate({
+      id: 'data.friends.castamere.description',
+      message: '代码美学',
+    }),
     href: 'https://www.castamerego.com',
     avatar: 'https://github.com/Casta-mere.png',
   },
   {
-    title: '吕舒君的博客',
-    description: '分享计算机学习过程中的相关知识、干活，不定期更新，欢迎常来看看哦！',
+    title: translate({
+      id: 'data.friends.cszj.title',
+      message: '吕舒君的博客',
+    }),
+    description: translate({
+      id: 'data.friends.cszj.description',
+      message: '分享计算机学习过程中的相关知识、干活，不定期更新，欢迎常来看看哦！',
+    }),
     href: 'https://cszj.wang',
     avatar: 'https://cszj.wang/api/f/X3fbn/mmexport1762511348709.png',
   },
   {
-    title: '愧怍',
-    description: '道阻且长，行则将至',
+    title: translate({
+      id: 'data.friends.kuizuo.title',
+      message: '愧怍',
+    }),
+    description: translate({
+      id: 'data.friends.kuizuo.description',
+      message: '道阻且长，行则将至',
+    }),
     href: 'https://kuizuo.me',
     avatar: 'https://kuizuo.me/img/logo.png',
   },
   {
-    title: 'SkyWT',
-    description: 'Passionate software designer & engineer.',
+    title: translate({
+      id: 'data.friends.skywt.title',
+      message: 'SkyWT',
+    }),
+    description: translate({
+      id: 'data.friends.skywt.description',
+      message: 'Passionate software designer & engineer.',
+    }),
     href: 'https://skywt.net',
     avatar: 'https://img.skywt.net/avatar.jpg',
   },
   {
-    title: '宝硕',
-    description: '即使是沉重的过去，也要接受它再继续向前迈进。',
+    title: translate({
+      id: 'data.friends.baoshuo.title',
+      message: 'Baoshuo',
+    }),
+    description: translate({
+      id: 'data.friends.baoshuo.description',
+      message: 'Even with a heavy past, accept it and continue moving forward.',
+    }),
     href: 'https://oi.baoshuo.ren',
     avatar: 'https://cdn.jsdelivr.net/npm/bsi/avatar/512x512.png',
   },
   {
-    title: "CyanHaze's Homepage",
-    description: 'Undergraduate @ ZJU',
+    title: translate({
+      id: 'data.friends.cyanHaze.title',
+      message: "CyanHaze's Homepage",
+    }),
+    description: translate({
+      id: 'data.friends.cyanHaze.description',
+      message: 'Undergraduate @ ZJU',
+    }),
     href: 'https://cyanhaze.github.io',
     avatar: 'https://cyanhaze.github.io/images/avatar.png',
   },
   {
-    title: "Studying Father's blog",
-    description: 'Focus on interest!',
+    title: translate({
+      id: 'data.friends.studyingFather.title',
+      message: "Studying Father's blog",
+    }),
+    description: translate({
+      id: 'data.friends.studyingFather.description',
+      message: 'Focus on interest!',
+    }),
     href: 'https://studyingfather.com',
     avatar: 'https://studyingfather.cdn.zsq001.cn/wp-content/uploads/2024/04/400x400.png',
+  },
+  {
+    title: translate({
+      id: 'data.friends.federico2903.title',
+      message: 'Federico2903',
+    }),
+    description: translate({
+      id: 'data.friends.federico2903.description',
+      message: 'Pisa tino, magi gali humo lonrodop.',
+    }),
+    href: 'https://www.federico2903.dev',
+    avatar: 'https://mydisk.cn-nb1.rains3.com/头像.png',
   },
 ];
